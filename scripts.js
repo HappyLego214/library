74f9075bd995b4cc21cc2551b490cb32688d098f
HappyLego214 <anthony21josh@gmail.com>
2023-06-05 04:41:58 +0800

    adds basic card display system

---
+++ scripts.js
@@ -0,0 +1,101 @@
+const characterCards = document.querySelectorAll(".character-card");
+const cardContainer = document.querySelector(".card-container");
+
+let library = []
+class char {
+    constructor(name, race, role, status) {
+        this.name = name
+        this.race = race
+        this.role = role
+        this.status = status
+    }
+
+    active() {
+        return (this.status ? this.status = "Active" : this.status = "Inactive");
+    }
+}
+
+function addCharToSelection(character) {
+    library.push(character);
+}
+
+function iterateThrough(array) {
+    for (let i = 0; i < array.length; i++) {
+        createCard(array[i].name, array[i].race, array[i].role);
+    }
+}
+
+function createCard(name, race, role) {
+
+    // CARD BODY
+
+    const newCard = document.createElement('div');
+    newCard.className = "character-card" ;
+    cardContainer.append(newCard);
+
+    const newStat = document.createElement('div');
+    newStat.className = "character-stats";
+    newCard.append(newStat);
+
+    const newView = document.createElement('div');
+    newView.className = "character-view";
+    newCard.append(newView);
+
+    // LIST VIEW
+
+    const newUL = document.createElement('ul');
+    newStat.append(newUL)
+
+    // NAME
+
+    const newNameLabel = document.createElement('label');
+    newNameLabel.textContent = "Name:";
+    newNameLabel.setAttribute('for', 'name');
+    newUL.append(newNameLabel);
+
+    const newName = document.createElement('li');
+    newName.className = 'name';
+    newName.textContent = name;
+    newUL.append(newName);
+
+    // RACE
+
+    const newRaceLabel = document.createElement('label');
+    newRaceLabel.textContent = 'Race:';
+    newRaceLabel.setAttribute('for', 'race');
+    newUL.append(newRaceLabel);
+
+    const newRace = document.createElement('li');
+    newRace.className = 'race';
+    newRace.textContent = race;
+    newUL.append(newRace);
+
+    // ROLE
+
+    const newRoleLabel = document.createElement('label');
+    newRoleLabel.textContent = 'Role:';
+    newRoleLabel.setAttribute('for', 'role');
+    newUL.append(newRoleLabel);
+
+    const newRole = document.createElement('li');
+    newRole.className = 'role';
+    newRole.textContent = role;
+    newUL.append(newRole);
+
+}
+
+// active character
+
+let ex = new char("TheLegend", "Orc", "Berserker", true)
+ex.active();
+
+// inactive character
+
+let ze = new char("ZzXt", "Human", "Archer", false)
+ze.active();
+
+
+addCharToSelection(ex);
+addCharToSelection(ze);
+
+iterateThrough(library);
